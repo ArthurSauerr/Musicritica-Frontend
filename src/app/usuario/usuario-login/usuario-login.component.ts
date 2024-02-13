16ce4f010101864fe22c9d3c52ca2ed20afe111b
ArthurSauerr <germanooarthuur@gmail.com>
2024-02-13 19:27:46 -0300

    Estilização e rotas de login e registro.

--- src/app/usuario/usuario-login/usuario-login.component.ts
+++ src/app/usuario/usuario-login/usuario-login.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { UsuarioService } from './../../shared/service/usuario.service';
 import { Component } from '@angular/core';
 
@@ -10,17 +11,22 @@ export class UsuarioLoginComponent {
   email: string;
   senha: string;
 
-  constructor(private usuarioService: UsuarioService) {}
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
   login() {
     this.usuarioService.login({ email: this.email, senha: this.senha }).subscribe(
       (response) => {
         // Login bem-sucedido, redirecione ou faça algo com o token JWT
+        this.router.navigate(['/']);
         console.log('Login bem-sucedido', response);
       },
       (error) => {
         console.error('Erro no login', error);
       }
     );
+  }
+
+  criarConta(){
+    this.router.navigate(['usuario/registrar']);
   }
 }
