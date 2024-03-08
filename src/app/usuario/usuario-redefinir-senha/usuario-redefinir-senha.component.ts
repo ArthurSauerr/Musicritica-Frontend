a2b52519f67aaed312f219d881d3405c36721a7d
ArthurSauerr <germanooarthuur@gmail.com>
2024-03-08 15:18:40 -0300

    feat: tela de redefinição de senha

--- src/app/usuario/usuario-redefinir-senha/usuario-redefinir-senha.component.ts
+++ src/app/usuario/usuario-redefinir-senha/usuario-redefinir-senha.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/shared/service/usuario.service';
 
 @Component({
   selector: 'app-usuario-redefinir-senha',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuario-redefinir-senha.component.scss']
 })
 export class UsuarioRedefinirSenhaComponent {
+
+  nome: string;
+  email: string;
+  senha: string;
+  confirmarSenha: string;
+
+  constructor(private usuarioService : UsuarioService, private router : Router){}
+
+  redefinirSenha(){
+    if(this.senha == this.confirmarSenha){
+      this.usuarioService.redefinirSenha({email: this.email, nome: this.nome, senha: this.senha}).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+  
+      )
+    } else{
+      console.log("Senhas não batem");
+    }
+  }
 
 }
