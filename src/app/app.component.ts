2775225f6f32915c3afdb21228c511299ca2ef39
ArthurSauerr <germanooarthuur@gmail.com>
2024-03-18 17:02:44 -0300

    feat: informações de usuario na tela de perfil e navegação por navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,34 @@
+import { UsuarioService } from './shared/service/usuario.service';
 import { DadosCompartilhadosService } from './shared/service/dados-compartilhados.service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
+import { Usuario } from './shared/model/Usuario';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Musicritica';
   mostrarNavbar: boolean = true;
+  emailParam: string | undefined;
+  idUsuario: number;
+  usuario: Usuario;
 
-  constructor(private router: Router, private dadosCompartilhadosService : DadosCompartilhadosService) {
+  constructor(
+    private router: Router,
+    private dadosCompartilhadosService: DadosCompartilhadosService,
+    private usuarioService: UsuarioService
+  ) {
     this.router.events.subscribe((event) => {
-      if (this.router.url.includes('/registrar') || this.router.url.includes('/login') || this.router.url.includes('/esqueceu-senha') || this.router.url.includes('/redefinir-senha')) {
+      if (
+        this.router.url.includes('/registrar') ||
+        this.router.url.includes('/login') ||
+        this.router.url.includes('/esqueceu-senha') ||
+        this.router.url.includes('/redefinir-senha')
+      ) {
         this.mostrarNavbar = false;
       } else {
         this.mostrarNavbar = true;
@@ -27,4 +42,45 @@ export class AppComponent {
     this.dadosCompartilhadosService.setMusicaProcurada(this.musicaProcurada);
     this.router.navigate(['/buscar']);
   }
+
+  verificarLogin() {
+    const token = this.usuarioService.getToken();
+
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const email = decodedToken.sub;
+      this.emailParam = email;
+      console.log('Email do usuario logado: ' + email);
+      this.buscarUsuarioLogado(email);
+    } else {
+      console.log('Token não encontrado.');
+    }
+  }
+
+  buscarUsuarioLogado(email: string | undefined) {
+    this.usuarioService
+      .buscarIdPorEmail(email)
+      .subscribe((idUsuario: number) => {
+        this.usuarioService.buscarUsuarioPorId(idUsuario).subscribe(
+          (usuario: Usuario) => {
+            this.usuario = usuario;
+            this.idUsuario = this.usuario.id;
+            console.log('Usuario buscado:', usuario);
+          },
+          (error) => {
+            console.error('Erro ao buscar usuário:', error);
+          }
+        );
+      });
+  }
+
+  verPerfilUsuario(){
+    if (this.idUsuario !== undefined && this.idUsuario !== null) {
+      // Navegar para a URL com o ID do usuário logado
+      this.router.navigate(['usuario/perfil', this.idUsuario]);
+    } else {
+      console.error('ID do usuário não está definido.');
+    }
+  }
+
 }
--- src/app/usuario/usuario-login/usuario-login.component.ts
+++ src/app/usuario/usuario-login/usuario-login.component.ts
@@ -2,6 +2,7 @@ import { DadosCompartilhadosService } from './../../shared/service/dados-compart
 import { Router } from '@angular/router';
 import { UsuarioService } from './../../shared/service/usuario.service';
 import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-usuario-login',
@@ -12,7 +13,7 @@ export class UsuarioLoginComponent {
   email: string;
   senha: string;
 
-  constructor(private usuarioService: UsuarioService, private router: Router) {}
+  constructor(private usuarioService: UsuarioService, private router: Router, private appComponent: AppComponent, private dadosCompartilhadosService: DadosCompartilhadosService) {}
 
   login() {
     this.usuarioService.login({ email: this.email, senha: this.senha }).subscribe(
@@ -21,6 +22,7 @@ export class UsuarioLoginComponent {
 
         this.router.navigate(['/']);
         console.log('Login bem-sucedido');
+        this.appComponent.verificarLogin();
       },
       (error) => {
         console.error('Erro no login', error);
