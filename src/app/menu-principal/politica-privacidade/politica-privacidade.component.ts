76e1c3c32dec2624396f049c1d559e900a9c6158
victorsaccucci <victorsaccucciv@gmail.com>
2024-07-04 08:13:37 -0300

    Feat política de privacidade

---
+++ src/app/menu-principal/politica-privacidade/politica-privacidade.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-politica-privacidade',
+  templateUrl: './politica-privacidade.component.html',
+  styleUrl: './politica-privacidade.component.scss'
+})
+export class PoliticaPrivacidadeComponent {
+
+}
